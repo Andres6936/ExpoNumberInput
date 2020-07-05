92152971923eb8fa56c570c573fffd8f58a4fc4e
Joan Andrés <andres6936@live.com>
2020-07-05 15:54:38 -0500

    Use of fat arrow function

--- src/App.js
+++ src/App.js
@@ -26,52 +26,49 @@ import logo from './logo.svg';
 // export default App;
 
 export default class App extends Component {
-  render () {
-    return (
-        <div className="container-fluid pt-5">
-            <main className="col-12 col-lg-4 bg-light mx-auto p-3">
-                <section className="row row-cols-2">
-                    <div className="col">
-                        <img src={logo} className="img-thumbnail" alt="Photo"/>
+  render = () =>
+    <div className="container-fluid pt-5">
+        <main className="col-12 col-lg-4 bg-light mx-auto p-3">
+            <section className="row row-cols-2">
+                <div className="col">
+                    <img src={logo} className="img-thumbnail" alt="Photo"/>
+                </div>
+
+                <div className="col">
+                    <div className="row form-inline border-bottom">
+                        <label className="col-4 col-form-label pr-5 border-right">Name:</label>
+                        <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="Joan"/>
                     </div>
 
-                    <div className="col">
-                        <div className="row form-inline border-bottom">
-                            <label className="col-4 col-form-label pr-5 border-right">Name:</label>
-                            <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="Joan"/>
-                        </div>
-
-                        <div className="row form-inline border-bottom">
-                            <label className="col-4 col-form-label pr-5 border-right">Age:</label>
-                            <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="23"/>
-                        </div>
-
-                        <div className="row form-inline border-bottom">
-                            <label className="col-4 col-form-label pr-5 border-right">Profession:</label>
-                            <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="Engineer System"/>
-                        </div>
-
-                        <div className="row form-inline border-bottom">
-                            <label className="col-4 col-form-label pr-5 border-right">Experience:</label>
-                            <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="2 years 4 months"/>
-                        </div>
-
-                        <div className="row form-inline border-bottom">
-                            <label className="col-4 col-form-label pr-5 border-right">Telephone:</label>
-                            <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="319 656 9458"/>
-                        </div>
+                    <div className="row form-inline border-bottom">
+                        <label className="col-4 col-form-label pr-5 border-right">Age:</label>
+                        <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="23"/>
                     </div>
-                </section>
 
-                <section>
+                    <div className="row form-inline border-bottom">
+                        <label className="col-4 col-form-label pr-5 border-right">Profession:</label>
+                        <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="Engineer System"/>
+                    </div>
 
-                </section>
+                    <div className="row form-inline border-bottom">
+                        <label className="col-4 col-form-label pr-5 border-right">Experience:</label>
+                        <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="2 years 4 months"/>
+                    </div>
 
-                <section>
+                    <div className="row form-inline border-bottom">
+                        <label className="col-4 col-form-label pr-5 border-right">Telephone:</label>
+                        <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value="319 656 9458"/>
+                    </div>
+                </div>
+            </section>
 
-                </section>
-            </main>
-        </div>
-    )
-  }
+            <section>
+
+            </section>
+
+            <section>
+
+            </section>
+        </main>
+    </div>
 }
