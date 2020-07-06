5a6af8ca317e68af8841fe8b3bd2af3f505d68cf
Joan Andrés <andres6936@live.com>
2020-07-05 21:14:48 -0500

    Fixed the style of add panel

--- src/AddApplicant.js
+++ src/AddApplicant.js
@@ -2,23 +2,23 @@ import React, {Component} from 'react';
 
 export class AddApplicant extends Component {
 
-    showLabel = (label, value) =>
+    showLabel = (label) =>
         <div className="row form-inline border-bottom">
             <label className="col-4 col-form-label pr-5 border-right">{label}</label>
-            <input className="col-8 form-control-plaintext pl-4" type="text" readOnly value={value}/>
+            <input className="col-8 form-control pl-4" type="text"/>
         </div>
 
     render = () =>
         <div className="container-fluid pt-5">
-            <main className="col-12 col-lg-4 bg-light mx-auto p-3">
+            <main className="col-12 col-lg-4 bg-light mx-auto p-3 border border-top-0">
                 <section className="row row-cols-1">
                     <div className="col">
 
-                        {this.showLabel("Name:", "Joan")}
-                        {this.showLabel("Age:", "23")}
-                        {this.showLabel("Profession:", "Engineer System")}
-                        {this.showLabel("Experience:", "2 years 4 months")}
-                        {this.showLabel("Telephone:", "319 656 9458")}
+                        {this.showLabel("Name:")}
+                        {this.showLabel("Age:")}
+                        {this.showLabel("Profession:")}
+                        {this.showLabel("Experience:")}
+                        {this.showLabel("Telephone:")}
 
                     </div>
                 </section>
