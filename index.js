b358bdf4c98c9455dab40673e19583f3d3cce602
Andres6936 <andres6936@gmail.com>
2025-05-28 09:37:16 -0500

    Remove NumericInput export and .npmignore
    This commit removes the default export of NumericInput and related utility constants. Additionally, the .npmignore file was deleted as it is no longer needed in the current structure.

--- index.js
+++
@@ -1,6 +0,0 @@
-import NumericInput from './packages/expo-number-input'
-import { DIMENSIONS } from './packages/expo-number-input/src/utils'
-export default NumericInput
-export {
-    DIMENSIONS
-}
