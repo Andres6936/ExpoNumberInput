c1b8edf93319f58b75c73780aea1610fcdcf76fb
Andres6936 <andres6936@gmail.com>
2025-05-27 23:30:28 -0500

    Refactor file structure for NumericInput and dependencies
    Relocated NumericInput and its related files to a new package directory: `packages/expo-number-input`. Updated import paths accordingly to reflect the new structure. Removed unused `Button/index.js` to streamline the codebase.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import NumericInput from './NumericInput'
-import { DIMENSIONS } from './utils'
+import NumericInput from './packages/expo-number-input'
+import { DIMENSIONS } from './packages/expo-number-input/utils'
 export default NumericInput
 export {
     DIMENSIONS
