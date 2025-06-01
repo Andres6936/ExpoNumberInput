e6c2b45f1bea12e4048cc38d832455ed81bdab82
Andres6936 <andres6936@gmail.com>
2025-05-31 21:51:24 -0500

    Add styling and configuration props to NumericInputMoreLess
    Enhanced `NumericInputMoreLess` to include customizable styles, such as border radius and width, with support for `up-down` and `plus-minus` input types. Introduced a `StyleSheet` for consistent styling and default behavior customization.

--- showcase/Input.tsx
+++ showcase/Input.tsx
@@ -1,8 +1,31 @@
 import * as NumericInput from 'expo-number-input'
+import {StyleSheet} from "react-native";
 
-export const NumericInputMoreLess = (props: NumericInput.Props) => {
+export const NumericInputMoreLess = (
+    {
+        totalWidth = 220,
+        borderColor = '#d4d4d4',
+        rounded = false,
+        ...props
+    }: NumericInput.Props
+) => {
+    const totalHeight = props.totalHeight ? props.totalHeight : (totalWidth * 0.4)
+    const borderRadiusTotal = totalHeight * 0.18
+
+    const inputContainerStyle = props.type === 'up-down' ?
+        [style.inputContainerUpDown, {
+            width: totalWidth,
+            height: totalHeight,
+            borderColor: borderColor
+        }, rounded ? {borderRadius: borderRadiusTotal} : {}, props.containerStyle] :
+        [style.inputContainerPlusMinus, {
+            width: totalWidth,
+            height: totalHeight,
+            borderColor: borderColor
+        }, rounded ? {borderRadius: borderRadiusTotal} : {}, props.containerStyle]
+
     return (
-        <NumericInput.Root>
+        <NumericInput.Root style={inputContainerStyle}>
             <NumericInput.NumericField {...props}/>
         </NumericInput.Root>
     )
@@ -11,3 +34,42 @@ export const NumericInputMoreLess = (props: NumericInput.Props) => {
 export const NumericInputUpDown = () => {
 
 }
+
+
+const style = StyleSheet.create({
+    seprator: {
+        backgroundColor: 'grey',
+        height: 80,
+    },
+    inputContainerUpDown: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+
+        borderColor: 'grey',
+        borderWidth: 1
+    },
+    inputContainerPlusMinus: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1
+    },
+    inputUpDown: {
+        textAlign: 'center',
+        padding: 0
+
+    },
+    inputPlusMinus: {
+        textAlign: 'center',
+        padding: 0
+    },
+    icon: {
+        fontWeight: '900',
+        backgroundColor: 'rgba(0,0,0,0)'
+    },
+    upDown: {
+        alignItems: 'center',
+        paddingRight: 15
+    }
+})
