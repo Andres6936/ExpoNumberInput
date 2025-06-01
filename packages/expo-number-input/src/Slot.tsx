a77d35eb84050278a7b6818d4726105af62b8f35
Andres6936 <andres6936@gmail.com>
2025-05-31 21:24:19 -0500

    Add Slot.View component to expo-number-input package
    Introduce Slot.View for enhanced element composition in React Native. Includes utilities for prop merging, ref composition, and style combination, inspired by Radix Primitives.

---
+++ packages/expo-number-input/src/Slot.tsx
@@ -0,0 +1,121 @@
+import * as React from 'react';
+import {
+    Image as RNImage,
+    Pressable as RNPressable,
+    Text as RNText,
+    View as RNView,
+    StyleSheet,
+    type PressableStateCallbackType,
+    type ImageProps as RNImageProps,
+    type ImageStyle as RNImageStyle,
+    type PressableProps as RNPressableProps,
+    type TextProps as RNTextProps,
+    type ViewProps as RNViewProps,
+    type StyleProp,
+} from 'react-native';
+import {Fragment} from "react";
+
+const View = function (props: React.ComponentPropsWithRef<typeof RNView> ) {
+    const {children, ref, ...viewSlotProps} = props;
+
+    if (!React.isValidElement(children)) {
+        console.error('Slot.View - Invalid asChild element', children);
+        return null;
+    }
+
+    return React.cloneElement<
+        React.ComponentPropsWithoutRef<typeof RNView>,
+        React.ComponentRef<typeof RNView>
+    >(
+        isTextChildren(children) ? <Fragment/> : children, {
+            ...mergeProps(viewSlotProps, children.props as AnyProps),
+            ref: ref ? composeRefs(ref, (children as any).ref) : (children as any).ref,
+        }
+    )
+}
+
+View.displayName = 'Slot.View';
+
+export {View};
+
+// This project uses code from WorkOS/Radix Primitives.
+// The code is licensed under the MIT License.
+// https://github.com/radix-ui/primitives/tree/main
+
+function composeRefs<T>(...refs: (React.Ref<T> | undefined)[]) {
+    return (node: T) =>
+        refs.forEach((ref) => {
+            if (typeof ref === 'function') {
+                ref(node);
+            } else if (ref != null) {
+                (ref as React.MutableRefObject<T>).current = node;
+            }
+        });
+}
+
+type AnyProps = Record<string, any>;
+
+function mergeProps(slotProps: AnyProps, childProps: AnyProps) {
+    // all child props should override
+    const overrideProps = { ...childProps };
+
+    for (const propName in childProps) {
+        const slotPropValue = slotProps[propName];
+        const childPropValue = childProps[propName];
+
+        const isHandler = /^on[A-Z]/.test(propName);
+        if (isHandler) {
+            // if the handler exists on both, we compose them
+            if (slotPropValue && childPropValue) {
+                overrideProps[propName] = (...args: unknown[]) => {
+                    childPropValue(...args);
+                    slotPropValue(...args);
+                };
+            }
+            // but if it exists only on the slot, we use only this one
+            else if (slotPropValue) {
+                overrideProps[propName] = slotPropValue;
+            }
+        }
+        // if it's `style`, we merge them
+        else if (propName === 'style') {
+            overrideProps[propName] = combineStyles(slotPropValue, childPropValue);
+        } else if (propName === 'className') {
+            overrideProps[propName] = [slotPropValue, childPropValue].filter(Boolean).join(' ');
+        }
+    }
+
+    return { ...slotProps, ...overrideProps };
+}
+
+type PressableStyle = RNPressableProps['style'];
+type ImageStyle = StyleProp<RNImageStyle>;
+type Style = PressableStyle | ImageStyle;
+
+function combineStyles(slotStyle?: Style, childValue?: Style) {
+    if (typeof slotStyle === 'function' && typeof childValue === 'function') {
+        return (state: PressableStateCallbackType) => {
+            return StyleSheet.flatten([slotStyle(state), childValue(state)]);
+        };
+    }
+    if (typeof slotStyle === 'function') {
+        return (state: PressableStateCallbackType) => {
+            return childValue ? StyleSheet.flatten([slotStyle(state), childValue]) : slotStyle(state);
+        };
+    }
+    if (typeof childValue === 'function') {
+        return (state: PressableStateCallbackType) => {
+            return slotStyle ? StyleSheet.flatten([slotStyle, childValue(state)]) : childValue(state);
+        };
+    }
+
+    return StyleSheet.flatten([slotStyle, childValue].filter(Boolean));
+}
+
+export function isTextChildren(
+    children: React.ReactNode | ((state: PressableStateCallbackType) => React.ReactNode),
+) {
+    return Array.isArray(children)
+        ? children.some(child => typeof child === 'string')
+        : typeof children === 'string';
+}
